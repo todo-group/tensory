69cd90a04ba59e6f2ae3bc09cc797269ea009b13
JyJyJcr <82190170+JyJyJcr@users.noreply.github.com>
2025-07-24 15:26:30 +0900

    docs: highest layer modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
-pub mod basic;
+/// core traits and structs low and middle layer implementation
 pub mod core;
+
+/// basical implementors
+pub mod basic;
+
+/// numerical dense tensor implemented with ndarray
+/// note: since ndarray is NOT aware of memory allocation and dynamic library call, this module will be replaced in the future by module `dense`, which is planned to call BLAS or other linear algebra libraries more directly.
 pub mod nd_dense;
