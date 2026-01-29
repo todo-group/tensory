959abea5ffffd8ff169bf12618d55f5f905b9e11
JyJyJcr <82190170+JyJyJcr@users.noreply.github.com>
2026-01-29 11:02:42 +0900

    chore: nix flake pin

--- flake.nix
+++ flake.nix
@@ -1,56 +1,56 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-jyjyjcr = {
+      url = "github:jyjyjcr/nix-jyjyjcr/dev/alt-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
 
-  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+  outputs = { nixpkgs, flake-utils, rust-overlay, nix-jyjyjcr, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ rust-overlay.overlays.default ];
-        };
-
-        shell-pkgs = [
-          (pkgs.rust-bin.stable.latest.default.override {
-            extensions = [ "rust-src" ];
-          })
-          # for openblas
-          pkgs.openblas
-          # for openblas 64-bit
-          # (pkgs.openblas.override { blas64 = true; })
-
-          # for blis (not checked yet)
-          # pkgs.blis
-
-          # for netlib
-          # pkgs.gfortran
-          # pkgs.gfortran.cc
-          # pkgs.blas-reference
-          # pkgs.lapack-reference
-
-          # for R
-          # pkgs.R
-          # pkgs.libintl
-
-          pkgs.pkg-config
-          pkgs.uv
-          pkgs.python313
-        ];
-        zshCompEnv = pkgs.buildEnv {
-          name = "zsh-comp";
-          paths = shell-pkgs;
-          pathsToLink = [ "/share/zsh" ];
+          overlays = [
+            rust-overlay.overlays.default
+            nix-jyjyjcr.overlays.${system}.default
+          ];
         };
       in {
-        devShells.default = pkgs.mkShell rec {
-          packages = shell-pkgs;
-          ZSH_COMP_FPATH = "${zshCompEnv}/share/zsh/site-functions";
+        devShells = pkgs.alt-shell.mkCommonShells { } {
+          packages = [
+            (pkgs.rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            })
+            # for openblas
+            pkgs.openblas
+            # for openblas 64-bit
+            # (pkgs.openblas.override { blas64 = true; })
+
+            # for blis (not checked yet)
+            # pkgs.blis
+
+            # for netlib
+            # pkgs.gfortran
+            # pkgs.gfortran.cc
+            # pkgs.blas-reference
+            # pkgs.lapack-reference
+
+            # for R
+            # pkgs.R
+            # pkgs.libintl
+
+            pkgs.pkg-config
+            pkgs.uv
+            pkgs.python313
+          ];
         };
       });
 }
